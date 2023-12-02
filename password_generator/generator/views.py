fa14b96fe1690874b92b70273033bdf65637576c
Maks328 <96689357+Maksim325@users.noreply.github.com>
2023-12-02 14:34:04 +0200

    add basic HTML and CSS

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -3,4 +3,5 @@ from django.http import HttpResponse
 import random
 
 def home(response):
-    return HttpResponse(random.randint(11111111, 99999999))
+    print(response.POST)
+    return render(response, 'generator/home.html' ,context={'password':random.randint(11111111, 99999999)})
