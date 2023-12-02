0d09b1b0dbdd1c057f3984bb693ae1d8edbed3d5
Maks328 <96689357+Maksim325@users.noreply.github.com>
2023-12-02 14:57:59 +0200

    add logic

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import random
+import string
+
+def generate_password(length=8):
+
+    characters = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(characters) for i in range(length))
+    return password
 
 def home(response):
+    length = response.POST['length']
     print(response.POST)
-    return render(response, 'generator/home.html' ,context={'password':random.randint(11111111, 99999999)})
+    if length == '':
+        length = 8
+    password = generate_password(int(length))
+    return render(response, 'generator/home.html' ,context={'password':password})
